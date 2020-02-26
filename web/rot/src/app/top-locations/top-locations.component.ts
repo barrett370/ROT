06089684af5f4f554319207340ef72a9f04685bc
Sam Barrett <barrett370@gmail.com>
2020-02-26 15:59:01 +0000

    tidy

--- web/rot/src/app/top-locations/top-locations.component.ts
+++ web/rot/src/app/top-locations/top-locations.component.ts
@@ -1,54 +1,41 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import locations from '../locations.json'
-import { HttpClient, HttpHeaders } from "@angular/common/http"
-
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import locations from "../locations.json";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 export interface OccupancyResult {
   occupancy: number;
 }
-  
 
 @Component({
-  selector: 'app-top-locations',
-  templateUrl: './top-locations.component.html',
-  styleUrls: ['./top-locations.component.css']
+  selector: "app-top-locations",
+  templateUrl: "./top-locations.component.html",
+  styleUrls: ["./top-locations.component.css"]
 })
-  
 export class TopLocationsComponent implements OnInit {
-
   corsHeaders = new HttpHeaders({
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'Access-Control-Allow-Origin': 'http://localhost:6969/'
-});
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:6969/"
+  });
   res: OccupancyResult;
-  async getOcc(id: number)  {
+  async getOcc(id: number) {
     let baseURL = "/api/occupancy/?buildingID=" + id;
-    // let baseURL = "https://example.com/"
-    // let headers = new HttpHeaders({ 'Access-Control-Allow-Origin': '*' })
     const result = await this.http.get<any>(baseURL).toPromise();
-    return result.occupancy
-
-
-    // return this.res.occupancy;
+    return result.occupancy;
   }
+
   construct_list() {
-    let ret = []
+    let ret = [];
     locations.forEach(async building => {
-      console.log(this.getOcc(building.id))
       const occ = await this.getOcc(building.id);
-      ret.push({name: building.name, percent: occ/building.max_occupancy})
+      ret.push({ name: building.name, percent: occ / building.max_occupancy });
     });
-    console.log(ret)
-    return ret
+    return ret;
   }
 
-  constructor(private http: HttpClient) { }
-  
-  public topLoc = this.construct_list()
+  constructor(private http: HttpClient) {}
 
+  public topLoc = this.construct_list();
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
