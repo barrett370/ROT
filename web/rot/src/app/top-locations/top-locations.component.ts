8d553ca1e45b91550c9e736762a8e4ad0755994f
Sam Barrett <barrett370@gmail.com>
2020-02-26 16:42:55 +0000

    working for top rooms, needs list slicing

--- web/rot/src/app/top-locations/top-locations.component.ts
+++ web/rot/src/app/top-locations/top-locations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import locations from "../locations.json";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { WebServerInterface, IDTypes } from "../shared/shared_functions.js";
 
 export interface OccupancyResult {
   occupancy: number;
@@ -12,27 +13,18 @@ export interface OccupancyResult {
   styleUrls: ["./top-locations.component.css"]
 })
 export class TopLocationsComponent implements OnInit {
-  corsHeaders = new HttpHeaders({
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:6969/"
-  });
-  res: OccupancyResult;
-  async getOcc(id: number) {
-    let baseURL = "/api/occupancy/?buildingID=" + id;
-    const result = await this.http.get<any>(baseURL).toPromise();
-    return result.occupancy;
-  }
-
   construct_list() {
     let ret = [];
     locations.forEach(async building => {
-      const occ = await this.getOcc(building.id);
+      const occ = await this.webServerInterface.getOccupancy(
+        building.id,
+        IDTypes.BID
+      );
       ret.push({ name: building.name, percent: occ / building.max_occupancy });
     });
     return ret;
   }
-
+  webServerInterface = new WebServerInterface(this.http);
   constructor(private http: HttpClient) {}
 
   public topLoc = this.construct_list();
--- web/rot/src/app/top-rooms/top-rooms.component.ts
+++ web/rot/src/app/top-rooms/top-rooms.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import locations from "../locations.json";
+import { WebServerInterface, IDTypes } from "../shared/shared_functions.js";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-top-rooms',
-  templateUrl: './top-rooms.component.html',
-  styleUrls: ['./top-rooms.component.css']
+  selector: "app-top-rooms",
+  templateUrl: "./top-rooms.component.html",
+  styleUrls: ["./top-rooms.component.css"]
 })
 export class TopRoomsComponent implements OnInit {
+  webServerInterface = new WebServerInterface(this.http);
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-  public topLoc  = [{name: 'Room 101', percent: 15}, {name: 'Room 202', percent: 20}, {name: 'Room 303', percent: 25}];
-  ngOnInit(): void {
+  construct_list() {
+    let ret = [];
+    locations.forEach(building => {
+      building.floors.forEach(floor => {
+        floor.rooms.forEach(async room => {
+          const occ = await this.webServerInterface.getOccupancy(
+            room.id,
+            IDTypes.RID
+          );
+          ret.push({ name: room.name, percent: occ / room.max_occupancy });
+        });
+      });
+    });
+    console.log(ret);
+    let sorted_ret:any[] = ret.sort((o1, o2) => {
+      if (o1.percent > o2.percent) {
+        return 1;
+      }
+      if (o1.percent < o2.percent) {
+        return -1;
+      }
+      return 0;
+    });
+    console.log(sorted_ret);
+    console.log(ret.slice(0, 4));
+    return ret
   }
 
+  // public topLoc = [
+  //   { name: "Room 101", percent: 15 },
+  //   { name: "Room 202", percent: 20 },
+  //   { name: "Room 303", percent: 25 }
+  // ];
+  public topLoc = this.construct_list();
+  ngOnInit(): void {}
 }
