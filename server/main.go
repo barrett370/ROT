b932b8e51b6d3a5c860263efa9b269f1fd4a89e8
Sam Barrett <barrett370@gmail.com>
2020-02-28 18:16:12 +0000

    minor logging tweak

--- server/main.go
+++ server/main.go
@@ -139,7 +139,7 @@ func (db *DB) calcOccupancy(w http.ResponseWriter, r *http.Request) {
 		response = Response{Occupancy: readings[0].Value}
 	}
 
-	fmt.Printf("%v\n", response)
+	fmt.Printf("%+v\n", response)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(response)
